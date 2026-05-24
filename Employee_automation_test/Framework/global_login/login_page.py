d53f89fe3acbaac91b059723985679b14749631a
kehinde Adamo <adamokennie@gmail.com>
2026-05-24 20:14:05 +0100

    global login

--- Employee_automation_test/Framework/global_login/login_page.py
+++ Employee_automation_test/Framework/global_login/login_page.py
@@ -1,17 +1,32 @@
-#By: help us to interact with the  elements on the pages 
 from selenium.webdriver.common.by import By
-class LoginPage:
-    def __init__(self,driver):
-        self.driver = driver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-    def open(self,base_url):
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 60)
+
+    def open(self, base_url):
         self.driver.get(base_url)
 
-    def login(self, username,password):
-        self.driver.find_element(By.NAME,"username").send_keys(username)
-        self.driver.find_element(By.NAME,"password").send_keys(password)
-        self.driver.find_element(By.CSS_SELECTOR,"button[type='submit']").click()
-
-
     
+        self.wait.until(
+            EC.presence_of_element_located((By.NAME, "username"))
+        )
+
+    def login(self, username, password):
         
+        username_input = self.wait.until(
+            EC.visibility_of_element_located((By.NAME, "username"))
+        )
+
+        password_input = self.wait.until(
+            EC.visibility_of_element_located((By.NAME, "password"))
+        )
+
+        username_input.send_keys(username)
+        password_input.send_keys(password)
+
+        
+        self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
